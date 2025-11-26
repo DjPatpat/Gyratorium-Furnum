2f41e308f38677ab9a92f90ec8f77ea12d18b419
DjPatpat <BlackPatrick160@gmail.com>
2025-11-26 13:50:52 +0100

    Added KebapALL.

---
+++ src/main/java/com/gf/items/KebapAll.java
@@ -0,0 +1,18 @@
+package com.gf.items;
+
+import com.gf.blocks.BlockPintsisEverfulKebap;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class KebapAll extends Item {
+    public SimpleTexturedItem() {
+        setRegistryName("simpletextureditem");
+        setUnlocalizedName(ModTut.MODID + ".simpletextureditem");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+}
